60d16857697dd9160602dd2a5c86cd4c01294c8f
luiz-ortega <luizngust@hotmail.com>
2022-02-06 04:29:00 -0300

    chore: remove unecessary config

--- mysql/CreateDatabase.sql
+++ mysql/CreateDatabase.sql
@@ -8,10 +8,3 @@ SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
-
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
